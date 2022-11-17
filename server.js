e27e5e5a32a888994a1562b012887307160b86c7
diego40g <diegopaznaula@hotmail.com>
2022-11-16 21:46:15 -0500

    add request Post example

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 8000;
+
+//Iniitalize middleware
+app.use(express.json({extended:false}))
 
 //Route test
 app.get('/', (req,res) => {
     res.send("Hola mundo!!!");
+});
+app.post('/',(req,res)=>{
+    res.send(`The request ${req.body.text}`)
 });
 
 app.listen(PORT, ()=>console.log(`Server started at port ${PORT}`));
